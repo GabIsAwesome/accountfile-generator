c56fa0dfb9f0727ccce2bd179b84522d5e456ca8
Gab <84043980+GabIsAwesome@users.noreply.github.com>
2024-01-29 19:36:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Account.dat value calculator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body class="container">
+   <h5>The account.dat file consists of various fields that are ESSENTIAL to connect online, mainly on <a href="//pretendo.network">Pretendo Network</a>. <br>  This tool focuses on trying to generate valid values to use on the file, therefore reviving the "Get your Cemu Files" option that was removed from the pretendo website.</h5>
+   <h6>Try out the <a href="/calculator/">Calculator</a> to get values from the following fields on account.dat: Uuid, PrincipalId, Mii Data and Mii Name.</h6>
+   <h6>Try out the <a href="/pass-hash/">pass-hash</a> page to get your AccountPasswordCache value.</h6>
+  </body>
+  </html>
