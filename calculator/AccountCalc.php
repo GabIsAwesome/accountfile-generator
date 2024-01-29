e1ed4c4167dd45684e5a40356bd0fcd6316bb881
Gab <84043980+GabIsAwesome@users.noreply.github.com>
2024-01-29 19:35:14 -0300

    Final updates

--- calculator/AccountCalc.php
+++ calculator/AccountCalc.php
@@ -6,23 +6,23 @@
     <title>Account.dat value calculator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
-  <body>
-
+  <body class="container">
+  <h1>Account.dat value calculator</h1>
+  <h5>By Gab</h5>
   <form method="POST">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Mii Name</label>
-    <input class="form-control" id="exampleInputEmail1" name="miiName" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <input class="form-control" id="exampleInputEmail1" name="miiName" aria-describedby="emailHelp" placeholder="Mii Name">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Mii Data</label>
-    <input class="form-control" name="miiData">
+    <input class="form-control" name="miiData" placeholder="Example: AwAAQEuX4+nGhzGy04Y5PxShgom0rwAAIkBwAHIAbwBkAHQAZQBzAHQAMQAAAEBAAAAhAQJoRBgmNEYUgRIXaA0AACkAUkhQSQBhAG4AAAAAAAAAAAAAAAAAAAAAAN2k">
   </div>
   <div class="mb-3">
   <label for="exampleInputPassword1" class="form-label">PID</label>
-    <input class="form-control" name="principalID">
+    <input type="number" class="form-control" name="principalID" placeholder="Example: 1799999999">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit!</button>
 </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -30,9 +30,10 @@
 </html>
 
     <?php
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
+    // Calculator
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    print '<h3>Here\'s the information you submitted, converted to valid values for use in the account.dat file.</h3><div class="card">
+    <div class="card-body">';
 
     if(isset($_POST["miiName"])) { 
     // Remember: if the Mii Name on the account.dat file is DIFFERENT from what the mii name is in the account server, only the name in the account server will be shown to others! Remember to change your mii's name so it can stay equal in both sides.
@@ -47,7 +48,7 @@
             $binaryMiiName .= $miiName[$i + 1] . $miiName[$i];
         }
         $binaryMiiName = str_pad($binaryMiiName, 0x16, "\0\0", STR_PAD_RIGHT); // Pad to 22 bytes with "00" at the end
-        echo "Mii Name:". bin2hex($binaryMiiName); // Should work on Cemu
+        print "Mii Name:". bin2hex($binaryMiiName); // Should work on Cemu
     }
 
     // Retrieve Mii Data from the post request
@@ -57,14 +58,14 @@
         // Decode base64-encoded Mii data
         $miiDataBinary = base64_decode($miiDataBase64);
 
-        echo "<br>Mii Data:". bin2hex($miiDataBinary);
+        print "<br>Mii Data:". bin2hex($miiDataBinary);
     }
 
     // Retrieve PID from the post request
     if(isset($_POST["principalID"])) {
         $accountPid = $_POST["principalID"];
         if (ctype_digit($accountPid)) {
-        echo "<br>PID:". dechex($accountPid); }
+        print "<br>PID:". dechex($accountPid); }
     }
 
         // Tries to generate a valid UUID.
@@ -88,7 +89,8 @@
       // Remove dashes to match the format
       $uuidString = str_replace('-', '', $uuid);
 
-      echo "<br>Uuid (generated randomly):". $uuidString;
+      print "<br>Uuid (generated randomly):". $uuidString;
 
+     print '<br><small><b>Still missing the AccountPasswordCache field? Don\'t worry, generate your hash in this <a href="/pass-hash/">page</a>.</b></small></div>';
     }
     ?>
